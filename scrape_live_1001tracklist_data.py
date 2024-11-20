7fce11e8173cb34276ac4bedad3d9eacd2fd79d9
byrnesy924 <b.byrnesy@gmail.com>
2024-11-20 22:40:28 +1100

    final edits and fixes to 1001 scraping script

--- scrape_live_1001tracklist_data.py
+++ scrape_live_1001tracklist_data.py
@@ -21,23 +21,28 @@ class OneThousandOneTrackListPage():
     def __init__(self, url: str, headers: dict, set_name: str, dj: str):
         self.base_url = "https://www.1001tracklists.com/"
         self.url = url
+        self.regex_clean_set_name = re.compile(r'\/|\||\#|\&|\(|\)|\"')
+        self.storage_file = self.regex_clean_set_name.sub("", set_name)
+        self.file_location = "1001_tracklist_htmls/" + self.storage_file + ".html"
 
         # 17 November - if found locally, then load locally, otherwise use response
-        if os.path.isfile("1001_tracklist_htmls/" + set_name + ".html"):
+        if os.path.isfile(self.file_location):
             try:
-                with open("1001_tracklist_htmls/" + set_name + ".html", "r", errors="replace") as data:
+                with open(self.file_location, "r", errors="replace") as data:
                     html_text = data.read()
+                    logger.info(f"Read data locally for set: {set_name}")
             except FileNotFoundError:
                 logger.exception(f"Response caused failure as file doesnt exist:\n\n{set_name}")
                 return
         else:
             # Otherwise, send request
+            sleep(1)  # trying not to get blacklisted by 1001 tacklists Would be great to async all this but I have not written it well
             # do this way as 1001 tracklists is liable to give 403s or 429s for many requests
             self.response = requests.get(url=self.base_url + url, headers=headers)  # move into function and pass into object
-
+            logger.info(f"No local file found for: {set_name}. Requesting data from 1001 tracklists")
             # Good status code - save data
             if self.response.status_code == 200:
-                with open("1001_tracklist_htmls/" + set_name.replace("/", "") + ".html", "w", errors="replace") as data:
+                with open(self.file_location, "w", errors="replace") as data:
                     # 1001 tracklist doesnt always play ball with lots of requests, so save locally
                     data.write(self.response.text)
                     html_text = self.response.text
@@ -143,7 +148,6 @@ def get_urls_for_boiler_rooms(headers: str):
 
 def multiprocessing_wrapper(set: dict, headers: dict):
     headers["path"] = set["url"]
-    sleep(3)
     return OneThousandOneTrackListPage(url=set["url"], set_name=set["Set"], headers=headers, dj=set["DJ"]).get_set_information()
 
 
@@ -164,7 +168,17 @@ if __name__ == "__main__":
         "method": "GET",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Connection": "keep-alive"
+        "Connection": "keep-alive",
+        "Cookie": "__qca=P0-820745957-1730510035946; guid=182cd3a595868; cookie=67f919b8-748c-4374-af2a-01fd8441f9ce; cookie_cst=zix7LPQsHA%3D%3D; _lr_env_src_ats=false; _cc_id=a939ad4a21d176c1613a23d09c2dd280; _au_1d=AU1D-0100-001730510032-PNJ3K11R-9Q0Z; ccuid=a0ce8c1d-1a64-4ce3-b6f8-4e340ac33f25; __browsiUID=3947c912-37f2-4c47-8b68-d4aa47c6dabc; __qca=P0-820745957-1730510035946; _ga=GA1.1.557609463.1730510128; panoramaId_expiry=1732246617834; panoramaId=244ad9e18af7acbe9d63ca051aba4945a7023f9f254678cec2c592fc994c43d7; panoramaIdType=panoIndiv; uuid=30CD4DF9-5B15-4E0D-923C-1F8621C78EBD; rl_anonymous_id=RS_ENC_v3_IjAzMDlkZDNiLTkxYjItNDQ5MS1iOWVhLTMzNTkxNjk3OWM0ZSI%3D; rl_page_init_referrer=RS_ENC_v3_IiRkaXJlY3Qi; rl_user_id=RS_ENC_v3_ImQzOGFmOTI0NzZjODM3NmMzMGYwYzE0NGMxMjk2NzhmZGU5ZTczNjQi; rl_session=RS_ENC_v3_eyJpZCI6MTczMTY0OTI3MDU5NSwiZXhwaXJlc0F0IjoxNzMxNjUxMDcwNjA2LCJ0aW1lb3V0IjoxODAwMDAwLCJhdXRvVHJhY2siOnRydWUsInNlc3Npb25TdGFydCI6dHJ1ZX0%3D; rl_trait=RS_ENC_v3_eyJydWRkZXJJZCI6IjJVQVBxY0tsYUFqTHQwa0poTTJ1YzNoWUpmNCIsImNhbXBhaWduIjoiMzI1NTkyMDAiLCJhZHYiOiIxMjUzNjMxNCIsInAiOiI0MDY0MTI5MjMiLCJjIjoiMjIzMDk1NTU1IiwiYWQiOiI1OTYzNDk0MDMiLCJlbnYiOiJqIiwicm5kIjoiMTAxMDM4NjUyOSIsInV1aWQiOiIxNTUyMGVmNi1mNjUwLTRjMzQtODdkMi1mZTY3ODEzMjE2MmEiLCJhY3Rpb24iOiJpbXByZXNzaW9uIiwib3JpZ2lucyI6Imh0dHBzOi8vd3d3LjEwMDF0cmFja2xpc3RzLmNvbSxodHRwczovL3d3dy4xMDAxdHJhY2tsaXN0cy5jb20ifQ%3D%3D; _lr_retry_request=true; connectId=%7B%22vmuid%22%3A%22nYw2SjpWLcI8tzkZFJO9t5AT_PFVjqVeI3HtjR8uRQ_zhq_s8Onlw0Nt7meczY2GNP0LS32mtcpfl2ROmRWZQA%22%2C%22connectid%22%3A%22nYw2SjpWLcI8tzkZFJO9t5AT_PFVjqVeI3HtjR8uRQ_zhq_s8Onlw0Nt7meczY2GNP0LS32mtcpfl2ROmRWZQA%22%2C%22connectId%22%3A%22nYw2SjpWLcI8tzkZFJO9t5AT_PFVjqVeI3HtjR8uRQ_zhq_s8Onlw0Nt7meczY2GNP0LS32mtcpfl2ROmRWZQA%22%2C%22ttl%22%3A86400000%2C%22lastSynced%22%3A1732013392484%2C%22lastUsed%22%3A1732013410284%7D; _ga_FVWZ0RM4DH=GS1.1.1732013352.7.1.1732013416.60.0.0; cto_bundle=tXSRZl9pOFI4aFE4N2ZrZG42UkVCVzZ5V3JuRFFNcDRaZEUlMkJ4MDFIZG52UWdzJTJGZ1B0dEQ3Vnl4ZUNvbktPRmVqQm1rdURZeGUxUkhZTHQwbVZRQnlJdHpJYUtBWVpsNW92S1ZYclpFUFFOVWdaOFRZVG05bjFKWXBmV3prM1lDeDElMkJqWGRWTjhoY0Jhenp1VDFOJTJCazNYJTJGUjNscjFBJTJCTUM1NWdmaXdqN1pwbiUyRlpkYyUzRA; __gads=ID=2d8730b0bb5c4846:T=1730510033:RT=1732016048:S=ALNI_MbmXAwUeYEfUNmb0EsfqQoSSLDLuw; __gpi=UID=00000f5d9c477e5a:T=1730510033:RT=1732016048:S=ALNI_Ma2FV8skRTR4UE1YsUgfJvV0E6tFA; __eoi=ID=398cba7e0c60d090:T=1730510033:RT=1732016048:S=AA-AfjY2swsSuc8pyzj5-wPCSGNa",
+        "dnt": "1",
+        "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "Windows",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1"
     }
 
     sets = get_urls_for_boiler_rooms(headers=headers)
@@ -177,4 +191,4 @@ if __name__ == "__main__":
     dfs = [multiprocessing_wrapper(set=br_set, headers=headers) for br_set in sets]
     df = pd.concat(dfs)
 
-    df.to_csv("1001_tracklist_set_lists.csv", index=False)
+    df.to_csv("1001_tracklist_set_lists.csv", index=False, encoding="utf-32")
